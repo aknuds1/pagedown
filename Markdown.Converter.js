9b95cff7e6cb58d18db741fc664770ab298518cf
Arve Knudsen <arve.knudsen@gmail.com>
2016-05-30 13:33:46 +0200

    Fix SoundCloud link generation

--- Markdown.Converter.js
+++ Markdown.Converter.js
@@ -766,7 +766,7 @@ if (typeof exports === 'object' && typeof require === 'function') {
     text = text.replace(/\[!soundcloud\]\s?\([ \t]*(\S+)[ \t]*\)/g, function (wholeMatch, trackId) {
       var result = '<iframe width="100%" height="450" scrolling="no" frameborder="no" ' +
         'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' +
-        '122498414&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;' +
+        trackId + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;' +
         'show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
       return result;
     });
